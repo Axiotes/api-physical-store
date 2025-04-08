6a490d89b5c5fe0eb755b2dd6ed4d1bd4bd63f6d
Arthur Axiotes <axiotesarthur@gmail.com>
2025-04-08 15:37:44 -0300

    feat: configuracao e scripts de migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,15 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      envFilePath: '.env',
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
