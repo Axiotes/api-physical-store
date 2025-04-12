1baf834e02210de7b7981b2d49365e3c6c1db7fb
Arthur Axiotes <axiotesarthur@gmail.com>
2025-04-12 11:02:13 -0300

    fix: ajuste no pipe de validacao do cep

--- src/common/pipes/cep-validation/cep-validation.pipe.ts
+++ src/common/pipes/cep-validation/cep-validation.pipe.ts
@@ -1,14 +1,14 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 
 @Injectable()
 export class CepValidationPipe implements PipeTransform {
   transform(value: string) {
     if (value.length !== 8) {
-      throw new Error(`CEP must be 8 digits`);
+      throw new BadRequestException(`CEP must be 8 digits`);
     }
 
     if (/[a-zA-Z]/.test(value)) {
-      throw new Error(`CEP must not have letters`);
+      throw new BadRequestException(`CEP must not have letters`);
     }
 
     return value;
