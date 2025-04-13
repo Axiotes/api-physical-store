2f0d75eb1f9094a1b8f3d7cddd5f7d7a24032d24
Arthur Axiotes <axiotesarthur@gmail.com>
2025-04-13 15:01:30 -0300

    feat: service para requisicao a api do melhor envio

--- src/common/requests/melhor-envio-api/melhor-envio-api.service.ts
+++ src/common/requests/melhor-envio-api/melhor-envio-api.service.ts
@@ -1,6 +1,8 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Observable } from 'rxjs';
+import { AxiosResponse } from 'axios';
 
 @Injectable()
 export class MelhorEnvioApiService {
@@ -17,21 +19,41 @@ export class MelhorEnvioApiService {
     }
   }
 
-  public freight(from: string, to: string) {
-    return this.httpService.post(
+  public freight(
+    from: string,
+    to: string,
+  ): Observable<AxiosResponse<MelhorEnvioResponse>> {
+    return this.httpService.post<MelhorEnvioResponse>(
       'https://melhorenvio.com.br/api/v2/me/shipment/calculate',
+      {
+        from: { postal_code: from },
+        to: { postal_code: to },
+        products: [
+          {
+            id: '1',
+            width: 15,
+            height: 10,
+            length: 20,
+            weight: 1,
+            insurance_value: 0,
+            quantity: 1,
+          },
+        ],
+        options: {
+          receipt: false,
+          own_hand: false,
+          insurance_value: 0,
+          reverse: false,
+          non_commercial: true,
+        },
+        services: ['1', '2'],
+        validate: true,
+      },
       {
         headers: {
           Authorization: `Bearer ${this.apiKey}`,
-        },
-        body: {
-          from: {
-            postal_code: from,
-          },
-          to: {
-            postal_code: to,
-          },
-          services: '1,2',
+          'Content-Type': 'application/json',
+          'User-Agent': 'PhysicalStore/1.0',
         },
       },
     );
